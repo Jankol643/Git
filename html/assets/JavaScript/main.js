e78ec9e1189293da9d9babf2baa19ad5407f440f
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2021-04-04 09:56:19 +0200

    added function to reset color of paragraphs

--- html/assets/JavaScript/main.js
+++ html/assets/JavaScript/main.js
@@ -1,20 +1,31 @@
+//adapted from https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/color
+
 var colorWell;
 var defaultColor = "#0000ff";
 
-window.addEventListener("load", startup, false);
+window.addEventListener('load', (event) => {
+    console.log('The page has fully loaded');
+	startup();
+	
+});
 
 function startup() {
+	console.log("This comes from startup!");
+	console.log("Calling colorPicker()");
 	colorPicker();
 }
 
 function colorPicker() {
 	colorWell = document.querySelector("#colorWell");
+	console.log(colorWell);
 	colorWell.value = defaultColor;
 	colorWell.addEventListener("input", updateFirst, false);
-	colorWell.addEventListener("change", updateAll, false);
+	colorWell.addEventListener("change", updateAll, false); // triggered when color picker changes
+
 	colorWell.select();
 }
 
+// changes first paragraph with class "changeColor" to color picker color
 function updateFirst(event) {
   var p = document.querySelector(".changeColor");
 
@@ -23,8 +34,16 @@ function updateFirst(event) {
   }
 }
 
+// changes all paragraphs with class "changeColor" to color picker color
 function updateAll(event) {
   document.querySelectorAll(".changeColor").forEach(function(p) {
     p.style.color = event.target.value;
   });
 }
+
+function resetAll() {
+  document.querySelectorAll(".changeColor").forEach(function(p) {
+    p.style.color = #000000; //black
+  });
+}
+
