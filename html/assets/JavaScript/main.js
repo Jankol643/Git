cd2731caa817a0ca5b773666c3e9886c0b24f786
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2021-04-05 06:27:38 +0200

    Added function for square root

--- html/assets/JavaScript/main.js
+++ html/assets/JavaScript/main.js
@@ -82,3 +82,69 @@ function expandCollapse(btn) {
 		}
 	});
 }
+
+function calc() {
+	let inputX = document.querySelector("#inputX").value;
+	inputX = parseFloat(inputX);
+	let inputY = document.querySelector("#inputY").value;
+	inputY = parseFloat(inputY);
+	let operation = document.querySelector('#arithoper option:checked').value
+	console.log("X is " + inputX);
+	console.log("Y is " + inputY);
+	console.log("Arithmetic is " + operation);
+	let test = errorCheck(inputX,inputY);
+	if (test == undefined) {
+		alert("Aborting");
+		return;
+	}
+	calcResult(inputX,inputY,operation);
+}
+
+function errorCheck(x,y) {
+	if ( (x == 0) && (y == 0) ) {
+		alert("Both fields must contain values!");
+		return undefined;
+	}
+	if ( (y == 0) && (operation == 'divide') ) {
+		alert("Cannot divide through zero!");
+		return undefined;
+	}
+	if ( (x < Number.MIN_SAFE_INTEGER) || (x > Number.MAX_SAFE_INTEGER) || (y < Number.MIN_SAFE_INTEGER) || (y > Number.MAX_SAFE_INTEGER) ) {
+		alert("The given numbers are too big to calculate with!");
+		return undefined;
+	}
+	if ( ((x*y < Number.MIN_SAFE_INTEGER) || (x*y > Number.MAX_SAFE_INTEGER)) && (operation == multiply) ) {
+		alert("Numbers are too big. Cannot multiply.");
+		return undefined;
+	}
+	if ( ((x/y < Number.MIN_SAFE_INTEGER) || (x/y > Number.MAX_SAFE_INTEGER)) && (operation == multiply) ) {
+		alert("Numbers are too big. Cannot divide.");
+		return undefined;
+	}
+	return x,y;
+}
+
+function calcResult(x,y,operation) {
+	let result;
+	switch (operation) {
+		case "add":
+			result = x + y;
+			console.log(x + " plus " + y + " is " + result);
+			break;
+		case "subtract":
+			result = x - y;
+			console.log(x + " minus " + y + " is " + result);
+			break;
+		case "multiply":
+			result = x * y;
+			console.log(x + " multiplied by " + y + " is " + result);
+			break;
+		case "divide":
+			result = x / y;
+			console.log(x + " divided by " + y + " is " + result);
+			break;
+		case "squareRoot":
+			result = Math.sqrt(x);
+			console.log("The square root of " + x + " is " + result);
+	}
+}
