de598be669a3b72cd8fd3a4defe8aac58410f168
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2021-09-28 07:51:26 +0200

    restructured code and enabled color reset button

--- html/assets/JavaScript/main.js
+++ html/assets/JavaScript/main.js
@@ -14,72 +14,76 @@ function startup() {
     colorPicker();
 }
 
-function colorPicker() {
+function colorPicker(type) {
     colorWell = document.querySelector("#colorWell");
-    console.log(colorWell);
     colorWell.addEventListener("input", updateFirst, false);
     colorWell.addEventListener("change", updateAll, false); // triggered when color picker changes
 
-    colorWell.select();
-}
-
-// changes first paragraph with class "changeColor" to color of color picker
-function updateFirst(event) {
-    let p = document.querySelector(".changeColor");
-
-    if (p) {
-        p.style.color = event.target.value;
+    // color reset on button click
+    if (type == "resetAll") {
+        resetAll();
     }
-}
 
-// changes all paragraphs with class "changeColor" to color picker color
-function updateAll(event) {
-    document.querySelectorAll(".changeColor").forEach(function (p) {
-        p.style.color = event.target.value;
-    });
-}
+    // changes first paragraph with class "changeColor" to color of color picker
+    function updateFirst(event) {
+        let p = document.querySelector(".changeColor");
 
-// Reset color of paragraphs to black
-function resetAll() {
-    document.querySelectorAll(".changeColor").forEach(function (p) {
-        p.style.color = "rgb(0,0,0)";
-    });
+        if (p) {
+            p.style.color = event.target.value;
+        }
+    }
+
+    // changes all paragraphs with class "changeColor" to color picker color
+    function updateAll(event) {
+        document.querySelectorAll(".changeColor").forEach(function (p) {
+            p.style.color = event.target.value;
+        });
+    }
+
+    // Reset color of paragraphs to black
+    function resetAll() {
+        document.querySelectorAll(".changeColor").forEach(function (p) {
+            p.style.color = "rgb(0,0,0)";
+        });
+    }
+
+    colorWell.select();
 }
 
 function expColBtn() {
     let btn = document.getElementById("expColBtn");
     expandCollapse(btn);
-}
 
-//Accordeon button open/collapse all
-function expandCollapse(btn) {
-    document.querySelectorAll(".accordion-collapse").forEach(function (collapse) {
-        if (collapse.classList.contains('show')) {
-            /* Accordion tab is open*/
-            btn.innerHTML = "Close Accordion";
-            collapse.classList.remove('show'); /* remove class "show" */
-            /*Setting properties for open/close button*/
-            document.querySelectorAll(".accordion-button").forEach(function (accButton) {
-                accButton.classList.add('collapsed');
-                /* Set aria-expanded to false */
-                let x = accButton.getAttribute("aria-expanded");
-                x = "true"
-                accButton.setAttribute("aria-expanded", x);
-            });
-        } else {
-            /* Accordion tab is closed*/
-            btn.innerHTML = "Open Accordion";
-            collapse.classList.add('show'); /* add class "show" */
-            /*Setting properties for open/close button*/
-            document.querySelectorAll(".accordion-button").forEach(function (accButton) {
-                accButton.classList.remove('collapsed');
-                /* Set aria-expanded to true */
-                x = accButton.getAttribute("aria-expanded");
-                x = "false"
-                accButton.setAttribute("aria-expanded", x);
-            });
-        }
-    });
+    //Accordeon button open/collapse all
+    function expandCollapse(btn) {
+        document.querySelectorAll(".accordion-collapse").forEach(function (collapse) {
+            if (collapse.classList.contains('show')) {
+                /* Accordion tab is open*/
+                btn.innerHTML = "Close Accordion";
+                collapse.classList.remove('show'); /* remove class "show" */
+                /*Setting properties for open/close button*/
+                document.querySelectorAll(".accordion-button").forEach(function (accButton) {
+                    accButton.classList.add('collapsed');
+                    /* Set aria-expanded to false */
+                    let x = accButton.getAttribute("aria-expanded");
+                    x = "true"
+                    accButton.setAttribute("aria-expanded", x);
+                });
+            } else {
+                /* Accordion tab is closed*/
+                btn.innerHTML = "Open Accordion";
+                collapse.classList.add('show'); /* add class "show" */
+                /*Setting properties for open/close button*/
+                document.querySelectorAll(".accordion-button").forEach(function (accButton) {
+                    accButton.classList.remove('collapsed');
+                    /* Set aria-expanded to true */
+                    x = accButton.getAttribute("aria-expanded");
+                    x = "false"
+                    accButton.setAttribute("aria-expanded", x);
+                });
+            }
+        });
+    }
 }
 
 function initCalc() {
@@ -488,5 +492,5 @@ function cutFirstLastChars() {
         }
         return line1;
     }
-    
+
 }
